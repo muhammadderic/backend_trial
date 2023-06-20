594874dc77e4ec3b291adfc25e2f439a8e4b7df4
muhammadderic <muhammad.donny.e@gmail.com>
2023-06-20 18:59:33 +0700

    Configure logging with winston

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const path = require("path");
+const winston = require('winston');
 const todoRoutes = require("./app/routes/todoRoutes");
 const { configureHelmet, configureCors } = require("./configs/security");
 
 const app = express();
 
+// Create the Winston logger
+const logger = winston.createLogger({
+  transports: [
+    // new winston.transports.Console(), // If you want show the logs in your console
+    new winston.transports.File({ filename: "logs/app.log" }),
+  ]
+})
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/public")));
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url} ${res.statusCode}`);
+  next();
+})
 
 // Security
 configureHelmet(app);
