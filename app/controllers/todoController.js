79d15cdf394a2c518968c592613d40ad6b5e17d5
muhammadderic <muhammad.donny.e@gmail.com>
2023-06-20 08:25:50 +0700

    Add logic to controller methods

--- app/controllers/todoController.js
+++ app/controllers/todoController.js
@@ -1,28 +1,71 @@
 const Todo = require("../models/todoModel");
 
 // POST a new todo
-const createTodo = (req, res) => {
-  res.send("post a new todo");
+const createTodo = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const newTodo = new Todo({ title, description });
+    const savedTodo = await newTodo.save();
+    res.status(201).json(savedTodo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 // GET all todos
-const getAllTodos = (req, res) => {
-  res.send("get all todos");
+const getAllTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 // GET a todo
-const getTodo = (req, res) => {
-  res.send("get a todo");
+const getTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ error: "Todo not found" })
+    }
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 // UPDATE a todo
-const updateTodo = (req, res) => {
-  res.send("update a todo");
+const updateTodo = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      { title, description },
+      { new: true }
+    )
+    if (!updatedTodo) {
+      return res.status(404).json({ error: "Todo not found" })
+    }
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 // DELETE a todo
-const deleteTodo = (req, res) => {
-  res.send("delete a todo");
+const deleteTodo = async (req, res) => {
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(
+      req.params.id
+    )
+    if (!deletedTodo) {
+      return res.status(404).json({ error: "Todo not found" })
+    }
+    res.json(deletedTodo)
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 module.exports = {
